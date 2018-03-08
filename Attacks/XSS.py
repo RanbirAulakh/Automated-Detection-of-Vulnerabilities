aac971b5a644c83493c21e0e1d8af368f54e9732
jad8251 <36778488+jad8251@users.noreply.github.com>
2018-03-07 21:06:59 -0500

    Update XSS.py
    Rough version that attempts to directly access a form with known fields on test website. Still doesn't work fully, as it's not able to submit the button completely (Probably failing a validation check somewhere, not quite sure). This is largely in RoboBrowser to experiment with it and see what can be done (Despite the lack of documentation), so this code will be subject to changes to bring it in line with other modules down the line.
    
    Also, the code above may not run if the whole thing is copied, just remove Class(xss): and it should work for local testing.

--- Attacks/XSS.py
+++ Attacks/XSS.py
@@ -1,1 +1,52 @@
 class XSS(object):
+  
+  
+  from bs4 import BeautifulSoup
+  import re
+  import urllib.request as urlreq
+  from robobrowser import RoboBrowser
+
+
+  def checkDVWAPage(URL):
+      page = urlreq.urlopen(URL)
+      soup = BeautifulSoup(page.read())
+
+      forms = soup.find_all('form')
+      for form in forms:
+          #print("Parsing form...")
+          localInputs = form.find_all('input')
+          localTextAreas = form.find_all('textarea')
+
+      browser = RoboBrowser()
+      browser.open(URL)
+      print("Starting URL: " + browser.url)
+      form = browser.get_form(method='post')
+      #Due to dvwa, need to login to get to target page
+      form['username'].value = 'admin'
+      form['password'].value = 'password'
+      form.serialize()
+      browser.submit_form(form)
+
+      #Should be on a new page now?
+      #Figure out what my current URL is
+      print(browser.url)
+      #Change URL
+      browser.open(URL)
+      print(browser.url)
+      XSSPageForms = browser.get_form(method='post')
+      print(XSSPageForms.fields)
+      #NOTE: name and message have character caps (Because bad website is bad)
+      XSSPageForms["txtName"].value = "PyScript"
+      XSSPageForms["mtxMessage"].text = 'Python Says Hello' #Magic injection
+      browser.submit_form(form, submit='btnClear')
+      #We are triggering the button, but we're still missing some proccess;
+      #Whatever we're saving isn't being stored- might be related to auth?
+
+
+
+  def testForms(formList):
+      #Somehow extract the forms using robobrowser and feed in samples from
+      #websites on how to test for XSS in forms (Not in links)
+      return
+
+  checkDVWAPage("http://127.0.0.1/dvwa/vulnerabilities/xss_s/")
