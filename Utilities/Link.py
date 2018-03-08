85991a572c5600630b5d985c067d36491b997f38
kemoy campbell <ksc2650@rit.edu>
2018-03-07 20:27:06 -0500

    fixed the merge issue with Link

--- Utilities/Link.py
+++ Utilities/Link.py
@@ -16,24 +16,12 @@ class Link(object):
     def addUrl(self,url):
         self.url = url
 
-    def addInput(self,input):
-<<<<<<< HEAD
-=======
-        if not isinstance(input,InputClass.Input):
-            raise Exception(input + " must be an instance of " + InputClass.Input)
->>>>>>> 1ddd9e4513fb6c8be3c955124bfbde87d767898c
-        self.inputs.append(input)
 
     def addContent(self,content):
         self.content = content
 
-    def getInputs(self):
-        return self.inputs
-
     def getContent(self):
         return self.content
 
-    def getUrl(self):
-        return self.url
 
 
