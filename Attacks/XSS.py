c915f0b872776b1ecbc743076eb0c30a4f364f8d
jad8251 <36778488+jad8251@users.noreply.github.com>
2018-03-29 16:23:44 -0400

    Update XSS.py
    Updated Stored attack to use same method as reflected attack.
    Stored attack works now, as it just scans the page for possible locations to submit POST requests, then scans the page after submitting to see if there's an exact match for it's injected text.
    Reflected attack currently has some issues, but this is partially due to how the POST didn't change the URL, so this may need to be addressed.

--- Attacks/XSS.py
+++ Attacks/XSS.py
@@ -9,21 +9,13 @@ class XSS(object):
 		
 		
 	#Takes in list of inputs that can be modified to store information
-	def attackStored(self, forms):
-		self.forms = forms
-		#for form in forms:
-			#Iterate through all possible forms using test inputs to see if we get a reaction. This is a bit harder to automate, as we have to update the page and check for specific reactions.
-		return
-        
-	def attackReflect(self, links):
-		print("TESTING REFLECTED XSS ATTACK")
+	def attackStored(self, links):
 		self.links = links
-		
-		#Load in examples of XSS scripts from some file, much like the Active SQL
-		#This is stored in as vectors? Similar to ActiveSQLInjection
-		
+		print("TESTING STORED XSS ATTACK")
+		#self.forms = forms
 		if self.links:
 			for link in self.links:
+				testScript = "<script>alert(123)</script>"
 				url = link.getUrl()
 				inputs = link.getInputs()
 				payload = {}
@@ -35,18 +27,64 @@ class XSS(object):
 						value = input.get('value')
 						
 						if not value:
+							value = testScript
 							#Set some default value 
-							value = "<script>alert(123)</script>"
+						#value = "<script>alert(123)</script>"
 							
 						payload[name] = value
 				
 				#Submit and test payload
 				print(payload)
 				query = self.request.post(url,data=payload)
+				print(query.url)
+                
+				#Check for script execution? 
+				print(query.text)
+				if testScript in query.text.lower():
+					print("XSS Stored Vulnerability found")
+		return
+        
+	def attackReflect(self, links):
+		print("TESTING REFLECTED XSS ATTACK")
+		self.links = links
+		
+		#Load in examples of XSS scripts from some file, much like the Active SQL
+		#This is stored in as vectors? Similar to ActiveSQLInjection
+		
+		if self.links:
+			for link in self.links:
+				testScript = "<script>alert(123)</script>"
+				url = link.getUrl()
+				inputs = link.getInputs()
+				payload = {}
 				
+				#Attempt to find something in all URLs that we can inject a script into
+				for input in inputs:
+					if input:
+						name = input.get('name')
+						value = input.get('value')
+						
+						if not value:
+							value = testScript
+							#Set some default value 
+						#value = "<script>alert(123)</script>"
+							
+						payload[name] = value
+				
+				#Submit and test payload
+				print(payload)
+				query = self.request.post(url,data=payload)
+				print(query.url)
+                
+				#Check for script execution? 
+				#print(query.text)
+				if testScript in query.text.lower():
+					print("XSS Reflected Vulnerability found")
+            
 				#Check result of running this on website, what do we get back?
-				#How do we determine the script ran?
-				print(type(query))
-				print(query.text.lower())
+				#We'll just match with whatever value we sent in, if we get it back,
+				#then the script ran.
+				#print(query.text.lower())
+			
 			
 			
