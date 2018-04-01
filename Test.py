bee2c438a34f7d252a2fe757c8efa1a3b40a47a2
jad8251 <36778488+jad8251@users.noreply.github.com>
2018-04-01 10:29:16 -0400

    Update Test.py
    Added rough tests for the XSS attacks, xssReflect doesn't work with the supplied /xss_r/ URL of dvwa, but stored works as intended.

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@ from Attacks.Fuzz import Fuzzer
 from Utilities.Requests import Requests
 from Attacks.ActiveSQLInjection import  ActiveSQLInjection
 from Attacks.PassiveSQLInjection import PassiveSQLInjection
+from Attacks.XSS import XSS
 from timeit import default_timer as timer
 from Attacks.BruteForce import BruteForce
 from Utilities import Link
@@ -26,19 +27,43 @@ from Utilities import Link
 
 url = "http://localhost/dvwa/login.php"
 
+
+
 request = Requests()
 request = request.request
 start = timer()
+
 b = BruteForce(url, request)
 flag, username, password, url = b.startBruteForce()
+
 end = timer()
 
-url = 'http://localhost/dvwa/vulnerabilities/brute/'
+url = 'http://localhost/dvwa/vulnerabilities/sqli/'
 fuzz = Fuzzer(request)
 fuzz.discover(url)
 
+
 activeSql = ActiveSQLInjection(request)
 activeSql.attack(fuzz.get_fuzz_links())
-
+"""
 passiveSql = PassiveSQLInjection(request)
-passiveSql.attack(fuzz.get_fuzz_links())
+passiveSql.attack(fuzz.get_fuzz_links())"""
+
+print("")
+
+#url = 'http://localhost/dvwa/vulnerabilities/xss_r/?name=<script>alert(123)<%2Fscript>#'
+url = 'http://localhost/dvwa/vulnerabilities/xss_r/'
+
+
+xfuzz = Fuzzer(request)
+xfuzz.discover(url)
+
+xss = XSS(request)
+xss.attackReflect(xfuzz.get_fuzz_links())
+
+url = 'http://localhost/dvwa/vulnerabilities/xss_s/'
+xrfuzz = Fuzzer(request)
+xrfuzz.discover(url)
+
+sxss = XSS(request)
+sxss.attackStored(xrfuzz.get_fuzz_links())
